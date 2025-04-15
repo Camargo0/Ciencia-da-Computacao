08770bd4985b5aa85fcf1a842cd7e79fbe2a9313
Camargo0 <144959255+Camargo0@users.noreply.github.com>
2025-04-15 10:59:26 -0300

    Delete Aeroporto directory

--- Aeroporto/Scripts/Triggers.sql
+++
@@ -1,8 +0,0 @@
--- Trigger para Monitorar Atualizações em Tickets
-CREATE TRIGGER TicketUpdateLog
-ON Tickets
-AFTER UPDATE
-AS
-BEGIN
-    PRINT 'A ticket has been updated.';
-END;
--- Aeroporto/Scripts/Views.sql
+++
@@ -1,4 +0,0 @@
--- View para Voos Disponíveis
-CREATE VIEW AvailableFlights AS
-SELECT FlightID, FlightNumber, Origin, Destination, DepartureTime, ArrivalTime, Airline, FlightStatus
-FROM Flights;
